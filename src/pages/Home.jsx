d86e92731e2f59b810f3782f7be4773c172d96a3
NISAMNIZ <muhammednisamk701@gmail.com>
2023-07-04 20:58:30 +0530

    third commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,33 @@
-// import React from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-// import imag from '../assets/spotify1.png'
+import './Home.css'
+import imag from '../assets/spotify.png'
 
 const Home = () => {
   return (
-    <div className='nav'>
-    <div className='image'>
-        {/* <img src={imag} alt="" /> */}
-    </div>
-    <div className='a'>Premium</div>
-    <div className='b'>Help</div>
-    <div className='c'>Download</div>
-    <div className='f'>Contact</div>
-    <Link to={"/signup"}>
+    <>
+    <div className='contai'>
+      <div className='nav'>
+      <div className='image'>
+        <img src={imag} alt="" />
+      </div>
+      <p>Spotify</p>
+      <div className='a'>Premium</div>
+      <div className='b'>Help</div>
+      <div className='c'>Download</div>
+      <Link to={"/signup"}>
         <div className='d'>Sign Up</div>
-    </Link>
-    <Link to={"/login"}>
-        <div className='e'>Log in</div>
-    </Link>
-
-
-    </div>
+      </Link>
+      <Link to={"/login"}>
+        <div className='e'>Log In</div>
+      </Link>
+    </div><div className="container">
+        <div className='sen1'>Music for Eveyone</div>
+        <div className='sen2'>Millions of songs.No credit card needed</div>
+        <button className='but'>GET SPOTIFY FREE
+        </button>
+      </div>
+      </div></>
   )
 }
 
